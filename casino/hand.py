77be5f7001e085168f02cb33c7ddc9417fdb9230
sameer Chauhan <48860013+sameer55chauhan@users.noreply.github.com>
2020-01-31 23:15:40 +0530

    Add files via upload

---
+++ casino/hand.py
@@ -0,0 +1,27 @@
+class Hand:
+    def __init__(self, dealer=False):
+        self.dealer = dealer
+        self.cards = []
+        self.values = 0
+
+    def add_cards(self, card):
+        self.cards.append(card)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == 'A':
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
